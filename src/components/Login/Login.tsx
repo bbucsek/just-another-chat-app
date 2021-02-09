a0c6e16ebdb0aa0b6785c71b0f6a904c9eccbf41
Barnabas, Bucsek <bucsek.barnabas@gmail.com>
2021-02-09 15:50:03 +0100

    feat: add style to login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { signIn } from '../../services/firestore/authentication'
 import { Button } from './styles'
-import { Wrapper, Title } from './styles'
+import { Container, Title, BarContainer, Bar, Wrapper } from './styles'
 
 export default function Login() {
 
@@ -11,10 +11,22 @@ export default function Login() {
   }
 
   return (
-    <Wrapper>
-      <Title>
-        just another chat app
-      </Title>
-      <Button onClick={handleClick}>Sing up with google</Button>
-    </Wrapper>)
+    <Container>
+      <Wrapper>
+        <Title>
+          just another chat app
+        </Title>
+        <Button onClick={handleClick}>Sing up with google</Button>
+      </Wrapper>
+      <BarContainer>
+        <Bar ownMessage={true} />
+        <Bar ownMessage={false} />
+        <Bar ownMessage={false} />
+        <Bar ownMessage={true} />
+        <Bar ownMessage={false} />
+        <Bar ownMessage={true} />
+        <Bar ownMessage={false} />
+        <Bar ownMessage={true} />
+      </BarContainer>
+    </Container>)
 }
--- src/components/Login/styles.ts
+++ src/components/Login/styles.ts
@@ -1,17 +1,25 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const Container = styled.div`
     height: 100vh;
+    width: inherit;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    flex-direction: row;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem;
 `
 
 export const Title = styled.div`
   color: ${({ theme }) => theme.colors.darkBlue};
   font-size: 3rem;
-
+  margin: 1rem;
 `
 
 export const Button = styled.button`
@@ -22,7 +30,7 @@ export const Button = styled.button`
   background-color: ${({ theme }) => theme.colors.darkBlue};
   color: ${({ theme }) => theme.colors.lightBlue};
   font-weight: bold;
-  min-width: 5rem;
+  max-width: 15rem;
   transition: all 0.2s ease-in-out;
   &:hover {
     transform: scale(1.1);
@@ -30,4 +38,23 @@ export const Button = styled.button`
   &:disabled {
     background-color: ${({ theme }) => theme.disabled};
   }
+`
+
+export const BarContainer = styled.div`
+  display: flex;
+  margin: 1rem;
+  flex: 1;
+  flex-direction: column;
+  width: inherit;
+`
+
+export const Bar = styled.div< { ownMessage: boolean } >`
+  display: flex;
+  margin: 1rem;
+  width: 200px;
+  height: 50px;
+  filter: blur(0.8px);
+  background: ${({ ownMessage, theme }) => ownMessage ? theme.colors.blue : theme.colors.darkBlue};
+  align-self: ${({ ownMessage }) => ownMessage ? "flex-end" : "flex-start"};
+  border-radius: 20px;
 `
